e20c84c91f637dbe5f88b8c3c78613b4763c6772
Matt Ball <matt@matt-ball.co.uk>
2019-08-27 17:11:37 -0700

    log vars

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
+    const myInput = core.getInput('collection');
+    const env = core.getInput('environment')
     core.debug(`Hello ${myInput}`);
+    console.log(env)
   } catch (error) {
     core.setFailed(error.message);
   }
